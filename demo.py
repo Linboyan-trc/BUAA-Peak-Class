ae347d7a4b2a84e88e5100bb9b8d7f63392360e8
Toryn <1584340372@qq.com>
2025-10-13 01:04:34 +0800

    try mlp

--- demo.py
+++ demo.py
@@ -115,7 +115,13 @@ def main():
     # 4. 基础推理
     model = load_model(model_name, attn_impl="sdpa", device=device)
     move_mlp_to_cpu(model)
-    run_and_log("Baseline (SPDA Attention)", model, tokenizer, long_prompt, device)
+    from torch.profiler import profile, ProfilerActivity
+    with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+                 record_shapes=True,
+                 profile_memory=True,
+                 with_stack=True) as prof:
+        run_and_log("Baseline (SPDA Attention)", model, tokenizer, long_prompt, device)
+    prof.export_chrome_trace("cpu_trace_mlp.json")
 
     # # 5. 做了KV-Cache优化的推理
     # replace_llama("streamingllm")
